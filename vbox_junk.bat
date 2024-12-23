2455b118b27907a0f124371de847025c78607025
dlandahl <landahl2014@gmail.com>
2024-12-23 13:57:05 +0000

    Folder struture

--- vbox_junk.bat
+++
@@ -1,9 +0,0 @@
-
-rm disk_image.vdi
-VBoxManage modifyvm new_os --hpet on
-VBoxManage convertfromraw --format VDI disk_image.img disk_image.vdi
-VBoxManage storageattach new_os --storagectl AHCI --port 0 --medium none --device 0
-VBoxManage closemedium disk_image.vdi
-VBoxManage storageattach new_os --storagectl AHCI --port 0 --medium disk_image.vdi --device 0 --type hdd
-VBoxManage setextradata new_os GUI/DefaultCloseAction PowerOff
-VBoxManage startvm new_os -E VBOX_GUI_DBG_ENABLED=true
